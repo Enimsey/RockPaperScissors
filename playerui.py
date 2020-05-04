7f6526422ae0b48cf14491806aff16e029fbbae4
Enimsey <61190627+Enimsey@users.noreply.github.com>
2020-05-04 18:21:48 +0100

    Update playerui.py

--- playerui.py
+++ playerui.py
@@ -1,25 +1,28 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'player.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.2
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-import socket
+from socket import *
+from main import POSSIBLE_CHOICES, POSSIBLE_CHOICES_LABELS
+from functools import partial
+import time
 
-class Ui_MainWindow(object):
-    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def is_relevant(decision):
+    # Tests if a string received from the server is a relevant decision
+    return "Draw" in decision or "wins" in decision
+
+
+class Player_UI(object):
+    socket = socket(AF_INET, SOCK_STREAM)
+    socket.settimeout(3)
     score = 0
-    opponentScore = 0
+    opponent_score = 0
     rounds = 0
     name = ""
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(300, 250)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+    buttons = {}
+
+    def setup_ui(self, main_window):
+        main_window.setObjectName("MainWindow")
+        main_window.setFixedSize(300, 250)
+        self.centralwidget = QtWidgets.QWidget(main_window)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 20, 270, 200))
@@ -32,23 +35,24 @@ class Ui_MainWindow(object):
         self.textEditPlayer = QtWidgets.QTextEdit(self.horizontalLayoutWidget)
         self.textEditPlayer.setObjectName("textEditPlayer")
         self.verticalLayout.addWidget(self.textEditPlayer)
-        self.pushButtonRock = QtWidgets.QPushButton(self.horizontalLayoutWidget)
-        self.pushButtonRock.setObjectName("pushButtonRock")
-        self.verticalLayout.addWidget(self.pushButtonRock)
-        self.pushButtonPaper = QtWidgets.QPushButton(self.horizontalLayoutWidget)
-        self.pushButtonPaper.setObjectName("pushButtonPaper")
-        self.verticalLayout.addWidget(self.pushButtonPaper)
-        self.pushButtonScissors = QtWidgets.QPushButton(self.horizontalLayoutWidget)
-        self.pushButtonScissors.setObjectName("pushButtonScissors")
-        self.verticalLayout.addWidget(self.pushButtonScissors)
+
+        # set the buttons for choices
+        for i in POSSIBLE_CHOICES:
+            self.buttons[i] = QtWidgets.QPushButton()
+            self.buttons[i].setObjectName("pushButton" + POSSIBLE_CHOICES_LABELS[i])
+            self.buttons[i].clicked.connect(partial(self.choice, i))
+            self.verticalLayout.addWidget(self.buttons[i])
+
         self.horizontalLayout.addLayout(self.verticalLayout)
-        
+
         self.verticalLayout2 = QtWidgets.QVBoxLayout()
         self.verticalLayout2.setObjectName("verticalLayout2")
-        self.textGameInteraction = QtWidgets.QTextEdit(self.horizontalLayoutWidget)
+
+        self.textGameInteraction = QtWidgets.QTextEdit()
         self.textGameInteraction.setObjectName("textGameInteraction")
         self.textGameInteraction.setEnabled(False)
         self.textGameInteraction.setPlainText("Welcome to Rock Paper Scissors")
+
         self.verticalLayout2.addWidget(self.textGameInteraction)
         self.pushButtonConnect = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.pushButtonConnect.setObjectName("pushButtonConnect")
@@ -59,144 +63,166 @@ class Ui_MainWindow(object):
         self.pushButtonConnect.clicked.connect(self.connect)
         self.pushButtonRestart.clicked.connect(self.restart)
         self.horizontalLayout.addLayout(self.verticalLayout2)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        main_window.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(main_window)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 219, 26))
         self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        main_window.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(main_window)
         self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        
-        self.pushButtonRock.clicked.connect(lambda: self.choice(0))
-        self.pushButtonPaper.clicked.connect(lambda: self.choice(1))
-        self.pushButtonScissors.clicked.connect(lambda: self.choice(2))
+        main_window.setStatusBar(self.statusbar)
 
         self.horizentalLayoutGameMode = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.radioButtonAgainstComputer = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButtonAgainstComputer.setChecked(True)
+        self.radioButtonAgainstComputer.setObjectName("radioButtonAgainstComputer")
         self.radioButtonTwoPlayer = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButtonTwoPlayer.setObjectName("radioButtonTwoPlayer")
         self.horizentalLayoutGameMode.addWidget(self.radioButtonAgainstComputer)
         self.horizentalLayoutGameMode.addWidget(self.radioButtonTwoPlayer)
+        self.horizentalLayoutGameMode.setObjectName("gameMode")
         self.verticalLayout2.addLayout(self.horizentalLayoutGameMode)
         self.restart()
 
-        
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        
-        
+        app.aboutToQuit.connect(lambda: self.close_window(main_window))
+        self.retranslate_ui(main_window)
+        QtCore.QMetaObject.connectSlotsByName(main_window)
 
-    def retranslateUi(self, MainWindow):
+    def retranslate_ui(self, main_window):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        main_window.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.textEditPlayer.setPlaceholderText(_translate("MainWindow", "Player name"))
-        self.pushButtonRock.setText(_translate("MainWindow", "Rock"))
-        self.pushButtonPaper.setText(_translate("MainWindow", "Paper"))
-        self.pushButtonScissors.setText(_translate("MainWindow", "Scissors"))
+        for i in POSSIBLE_CHOICES:
+            self.buttons[i].setText(_translate("MainWindow", POSSIBLE_CHOICES_LABELS[i]))
         self.pushButtonConnect.setText(_translate("MainWindow", "Connect"))
         self.pushButtonRestart.setText(_translate("MainWindow", "Restart"))
         self.radioButtonAgainstComputer.setText("1")
         self.radioButtonTwoPlayer.setText("2")
 
     def connect(self):
-        if(len(self.textEditPlayer.toPlainText()) != 0):
-            hote = "localhost"
+        # Callback for the connect button
+        # Connects to the server if it is up
+        if len(self.textEditPlayer.toPlainText()) != 0:
+            host = "localhost"
             port = 1111
-            self.name = self.textEditPlayer.toPlainText()
-            playerInformation = str(self.radioButtonAgainstComputer.isChecked())+"@"+self.name
             try:
-                self.socket.connect((hote, port))
-                self.socket.send(playerInformation.encode("Utf8"))
-                self.pushButtonRock.setDisabled(False)
-                self.pushButtonPaper.setDisabled(False)
-                self.pushButtonScissors.setDisabled(False)
+                self.socket.connect((host, port))
+                self.name = self.textEditPlayer.toPlainText()
+                player_information = str(self.radioButtonAgainstComputer.isChecked()) + "@" + self.name
+                self.socket.send(player_information.encode("Utf8"))
                 if self.radioButtonAgainstComputer.isChecked():
                     self.textGameInteraction.setPlainText("Connected: play against Computer")
                 else:
-                    serverResponse = ""
-                    while serverResponse == "":
-                        serverResponse = self.socket.recv(255)
-                    self.textGameInteraction.setPlainText(serverResponse.decode(encoding="utf-8")+"\n")
-                    
+                    self.handle_two_player_mode_connection()
+
+                for i in POSSIBLE_CHOICES:
+                    self.buttons[i].setDisabled(False)
+
+                self.pushButtonConnect.setDisabled(True)
+                self.textEditPlayer.setDisabled(True)
             except:
                 self.textGameInteraction.setPlainText("Unable to connect to server")
-                
-            
-    
-    def choice(self, playerChoice):
-        name = self.textEditPlayer.toPlainText()
-        choiceStr = name + '@' + str(playerChoice)
-        try:          
-            self.socket.send(choiceStr.encode("Utf8"))
-            # Disable the name/game mode editing
-            self.radioButtonTwoPlayer.setDisabled(True)
-            self.radioButtonAgainstComputer.setDisabled(True)
-            self.textEditPlayer.setDisabled(True)
+        else:
+            self.textGameInteraction.setPlainText("Player name is mandatory")
+
+    def handle_two_player_mode_connection(self):
+        # Expects the server response when we connect
+        server_response = ""
+        while server_response == "":
+            server_response = self.socket.recv(255).decode(encoding="utf-8")
+
+        self.textGameInteraction.setPlainText(server_response + "\n")
+
+    def choice(self, player_choice):
+        # Callback for all the possible choices buttons
+        # Send the player's choice to the server in the following form: name@choice
+        # Expects and handles the response of the server
+        choice_str = self.name + '@' + str(player_choice)
+        try:
+            self.socket.send(choice_str.encode("Utf8"))
         except:
-            print("unable to send choice, try again")
-         
-        self.handleDecision()
-        
-        
-    def handleDecision(self):
+            self.textGameInteraction.setPlainText("Connection with server lost")
+            return
+
+        # Disable the name/game mode editing
+        self.radioButtonTwoPlayer.setDisabled(True)
+        self.radioButtonAgainstComputer.setDisabled(True)
+        self.textEditPlayer.setDisabled(True)
+        # Disable sending another choice
+        for i in POSSIBLE_CHOICES:
+            self.buttons[i].setDisabled(True)
+
+        self.handle_decision()
+
+        # Enable sending another choice
+        for i in POSSIBLE_CHOICES:
+            self.buttons[i].setDisabled(False)
+
+    def handle_decision(self):
+        # Expects and handles the response of the server when a choice has been made by the player
         decision = self.socket.recv(255).decode(encoding="utf-8")
         if self.radioButtonAgainstComputer.isChecked():
-            while not(self.isRelevent(decision)):
+            while not (is_relevant(decision)):
                 decision = self.socket.recv(255).decode(encoding="utf-8")
             self.rounds += 1
             if decision != "Draw":
                 if self.name in decision:
                     self.score += 1
                 else:
-                    self.opponentScore += 1
-            self.textGameInteraction.clear()     
-            self.textGameInteraction.setPlainText(decision + "\nscore: "+str(self.score) + " against " + str(self.opponentScore)+"\n"+"Number of rounds: "+ str(self.rounds))
+                    self.opponent_score += 1
+            self.textGameInteraction.clear()
+            self.textGameInteraction.setPlainText(decision + "\nscore: " + str(self.score) + " against " + str(
+                self.opponent_score) + "\n" + "Number of rounds: " + str(self.rounds))
             return
         if self.radioButtonTwoPlayer.isChecked():
-            while not(self.isRelevent(decision)):
-                print(decision)
+            t0 = time.time()
+            delta = 20  # wait 20 maximum seconds for decision
+            while not (is_relevant(decision)):
                 decision = self.socket.recv(255).decode(encoding="utf-8")
+                if delta < time.time() - t0:
+                    print("Unable to get decision")
+                    return
             self.rounds += 1
             if decision != "Draw":
                 if self.name in decision:
                     self.score += 1
                 else:
-                    self.opponentScore += 1
-            self.textGameInteraction.clear()     
-            self.textGameInteraction.setPlainText(decision + "\nscore: "+str(self.score) + " against " + str(self.opponentScore)+"\n"+"Number of rounds: "+ str(self.rounds))
-             
-    
-    def isRelevent(self,decision):
-        return "Draw" in decision or "wins" in decision
-    
-    def restart(self):
-        self.textEditPlayer.setDisabled(False)
-        self.textEditPlayer.clear()
+                    self.opponent_score += 1
+            self.textGameInteraction.clear()
+            self.textGameInteraction.setPlainText(decision + "\nScore: " + str(self.score) + " against " + str(
+                self.opponent_score) + "\n" + "Number of rounds: " + str(self.rounds))
 
-        self.pushButtonRock.setDisabled(True)
-        self.pushButtonPaper.setDisabled(True)
-        self.pushButtonScissors.setDisabled(True)
-        
-        self.radioButtonTwoPlayer.setCheckable(True)
-        self.radioButtonAgainstComputer.setCheckable(True)
-        self.radioButtonAgainstComputer.setChecked(True)
-        
+    def restart(self):
+        # Callback for the restart button
+        # Re-initializes the socket and all variables related to a game
+        self.textEditPlayer.setDisabled(False)
+        self.pushButtonConnect.setDisabled(False)
+
+        for i in POSSIBLE_CHOICES:
+            self.buttons[i].setDisabled(True)
+
         self.socket.close()
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket(AF_INET, SOCK_STREAM)
         self.textGameInteraction.setPlainText("Welcome to Rock Paper Scissors")
         self.score = 0
-        self.opponentScore = 0
-        self.rounds = 0        
-        
-        
-        
-        
+        self.opponent_score = 0
+        self.rounds = 0
+
+        self.radioButtonTwoPlayer.setDisabled(False)
+        self.radioButtonAgainstComputer.setDisabled(False)
+
+    def close_window(self, dialog):
+        # Callback for the close window button
+        self.socket.close()
+        dialog.close()
+
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    ui = Player_UI()
+    ui.setup_ui(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
